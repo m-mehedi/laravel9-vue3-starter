9ed17c9d5ba37bd82cfa2b69c4b9d1cb67a849dd
MEHEDI <me.mehedi@hotmail.com>
2022-04-20 15:21:31 +0600

    Navigation, Dashboard and AUTH setup

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,30 +1,10 @@
-<script>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import {mapState} from 'vuex'
+<script setup>
 
-export default {
-  components:{
-    HelloWorld
-  },
-  computed: {
-    ...mapState(['user'])
-  }
-}
 </script>
 
 <template>
-  <HelloWorld msg="DSR" />
+  <router-view></router-view>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
